98c0333b41065b7bc4b0ac71f2a6ba4d0c7080f1
gengjihui_maya@163.com <gengjihui_maya@163.com>
2020-02-15 09:47:53 +0800

    技术更新：完成日志交易记录代码修改

--- src/com/geng/service/impl/LogServiceImpl.java
+++ src/com/geng/service/impl/LogServiceImpl.java
@@ -1,5 +1,6 @@
 package com.geng.service.impl;
 
+import com.geng.mapper.LogMapper;
 import com.geng.pojo.Log;
 import com.geng.service.LogService;
 import com.geng.utils.PageHelper;
@@ -19,6 +20,16 @@ import java.util.Map;
  * 异常在service抛
  */
 public class LogServiceImpl implements LogService {
+    private LogMapper logMapper;
+
+    public LogMapper getLogMapper() {
+        return logMapper;
+    }
+
+    public void setLogMapper(LogMapper logMapper) {
+        this.logMapper = logMapper;
+    }
+
     /**
      * 分页
      *
@@ -29,22 +40,13 @@ public class LogServiceImpl implements LogService {
      */
     @Override
     public PageHelper showPage(int pageSize, int pageNumber) throws IOException {
-        //先获取mybatis全局配置,转换字节流，以便接下来交给SqlSessionFactoryBuilder使用
-        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
-        //获得sqlSessionFactory,建造者模式
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-        //获得sqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        Map<String, Object> map = new HashMap<>();
-        //分页查询
-        map.put("pageStart", pageSize * (pageNumber - 1));//起始页
-        map.put("pageSize", pageSize);//一页显示几行
+        int pageStart = pageSize * (pageNumber - 1);
         //查询log日志
-        List<Log> list = sqlSession.selectList("com.geng.mapper.LogMapper.selectPage", map);
-        int total = sqlSession.selectOne("com.geng.mapper.LogMapper.selectCount");
+        List<Log> logList = logMapper.selectPage(pageStart, pageSize);
+        int total = logMapper.selectCount();
         PageHelper pageHelper = new PageHelper();
         //放入日志list
-        pageHelper.setList(list);
+        pageHelper.setList(logList);
         //放入当前页面
         pageHelper.setPageNumber(pageNumber);
         //放入每页显示行数
@@ -52,5 +54,20 @@ public class LogServiceImpl implements LogService {
         //总行数
         pageHelper.setTotal(total % pageSize == 0 ? total / pageSize : total / pageSize + 1);
         return pageHelper;
+
+//        //先获取mybatis全局配置,转换字节流，以便接下来交给SqlSessionFactoryBuilder使用
+//        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+//        //获得sqlSessionFactory,建造者模式
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+//        //获得sqlSession
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//        Map<String, Object> map = new HashMap<>();
+//        //分页查询
+//        map.put("pageStart", pageSize * (pageNumber - 1));//起始页
+//        map.put("pageSize", pageSize);//一页显示几行
+
+//        List<Log> list = sqlSession.selectList("com.geng.mapper.LogMapper.selectPage", map);
+        //        int total = sqlSession.selectOne("com.geng.mapper.LogMapper.selectCount");
+
     }
 }
